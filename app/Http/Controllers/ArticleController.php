b6a0209176d58c80903b987b3e602b2e6114b834
manueljun17 <manuelmanansalawork@gmail.com>
2019-10-11 01:28:09 +0800

    first commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Article;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $article = Article::paginate(10);
+        return response()->json($article, 200);
+    }
+
+    public function show(Article $article)
+    {
+        return response()->json($article, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $article = Article::create($request->all());
+
+        return response()->json($article, 201);
+    }
+
+    public function update(Request $request, Article $article)
+    {
+        $article->update($request->all());
+
+        return response()->json($article, 200);
+    }
+
+    public function delete(Article $article)
+    {
+        $article->delete();
+
+        return response()->json(null, 204);
+    }
+}
